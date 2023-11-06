881c4b48a7f3bcbd945993bde6cc11c111960730
Ozgur Ozhan <ozgur2096@gmail.com>
2023-11-06 17:26:12 +0100

    I will continue later

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,9 @@ class Project {
 }
 
 // Project State Management
+type Listener = (items: Project[]) => void;
 class ProjectState {
-  private listeners: any[] = [];
+  private listeners: Listener[] = [];
   private projects: Project[] = [];
   private static instance: ProjectState;
 
@@ -30,7 +31,7 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
@@ -116,7 +117,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
     });
